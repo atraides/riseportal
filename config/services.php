780b5c11bb0f9b63f1afb8418856f3dc323758da
atraides <d.hagyarossy@sapstar.eu>
2017-06-09 14:14:54 +0200

    Added support for the region in the Battle.net OAuth provider

--- config/services.php
+++ config/services.php
@@ -39,6 +39,7 @@ return [
     'client_id' => env('BATTLENET_KEY'),
     'client_secret' => env('BATTLENET_SECRET'),
     'redirect' => env('BATTLENET_REDIRECT_URI'),  
+    'region' => env('BATTLENET_REGION'),
     ], 
 
 ];
