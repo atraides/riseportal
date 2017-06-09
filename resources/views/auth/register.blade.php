f26cb95080e8824888c0bc72a2e876ea1e71a1db
atraides <d.hagyarossy@sapstar.eu>
2017-06-09 14:39:20 +0200

    Centered the registration button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,9 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
                 <div class="panel-body">
-                    <a href="{{ route('bnetauth') }}" class="btn btn-primary">Register with Battle.net</a>
+                    <div class="text-center">
+                        <a href="{{ route('bnetauth') }}" class="btn btn-primary">Register with Battle.net</a>
+                    </div>
                 </div>
             </div>
         </div>
