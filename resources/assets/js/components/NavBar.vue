98f1db36faae12be7a4b75970733bae73f91dc45
atraides <d.hagyarossy@sapstar.eu>
2017-06-19 19:26:55 +0200

    Final polish before first release

--- resources/assets/js/components/NavBar.vue
+++ resources/assets/js/components/NavBar.vue
@@ -7,13 +7,10 @@
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav my-auto mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/home">Home</a>
           </li>
           <li class="nav-item" v-if="signedIn">
-            <a class="nav-link" href="#" @click="showRLPVote">RLP</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Disabled</a>
+            <a class="nav-link" href="#" @click="showRLPVote">Szavazás</a>
           </li>
         </ul>
         <!-- Authentication Links -->
